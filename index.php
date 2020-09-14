848492e0ebae63ed31b2963029dd4e5db02f9d93
zol <31492461+zol22@users.noreply.github.com>
2020-09-14 15:08:57 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$zip = $_GET["zip"];
+$url = "http://api.openweathermap.org/data/2.5/weather?zip=".$zip.","."us&units=imperial&appid=a8257db81643c8c6d894e79d283df895"; 
+function display($url)
+{
+
+$fp = fopen ( $url , "r" ) or die ("Could not contact $url"); 
+$contents = "";
+while ( $more = fread ( $fp, 1000  ) ) {      $contents .=  $more ;   }
+echo $contents ; 
+
+}
+display ($url );
+?>
+
+
+
+	
